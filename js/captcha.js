33c6a0510eeba47b1a6289668757b4414be24228
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-01-02 18:32:53 +0700

    feature: initial

--- js/captcha.js
+++ js/captcha.js
@@ -1,14 +1,14 @@
 async function moCreateCaptchaQuestion()
 {
 	const isUserSessionExists = moCheckUserSessionExists();
-	
+
 	if (isUserSessionExists) return 1;
 
 
-	const response = await moFetchApi('captcha/createQuestion', options);
-	
+	const response = await moFetchApi('captcha/createQuestion');
+
 	var resultCode = response.headers.get('Mo-Captcha-Result-Code');
-	
+
 	if (resultCode == null) throw new Error(response.headers);
 
 	if (resultCode == '11') return 1;
@@ -21,20 +21,67 @@ async function moCreateCaptchaQuestion()
 	const holder = getElement('captchaHolder');
 
 	const text = await moFetchText('/html/captcha.html');
-	
-	-;
-	if (text == null)
-	{
-		holder.innerHTML = '!';
-		
-		return;
-	}
-	
-	holder.innerHTML = text;
-	
-	
-	const objectURL = URL.createObjectURL(blob);
-	
-	image.src = objectURL;
 
+	holder.innerHTML = text;
+
+
+	const imageElement = getElement('captchaImage');
+
+	const objectURL = URL.createObjectURL(blob);
+
+	imageElement.src = objectURL;
+
+	imageElement.dataset.degree = 0;
+
+
+	const decreaseDegreeElement = getElement('captchaDecreaseDegree');
+
+	decreaseDegreeElement.addEventListener('click', decreaseDegree);
+
+
+	const increaseDegreeElement = getElement('captchaIncreaseDegree');
+
+	increaseDegreeElement.addEventListener('click', increaseDegree);
+
+
+	const submitElement = getElement('captchaSubmit');
+
+	submitElement.addEventListener('click', submitCaptcha);
+
+	return 0;
+
+
+	function decreaseDegree()
+	{
+		var degree = imageElement.dataset.degree - 45;
+
+		if (degree < 0) degree = 315;
+
+		imageElement.dataset.degree = degree;
+
+		decreaseDegreeElement.style.transform = 'rotate(' + (degree - 180) + 'deg)';
+	}
+
+	function increaseDegree()
+	{
+		var degree = imageElement.dataset.degree + 45;
+
+		if (degree > 315) degree = 0;
+
+		imageElement.dataset.degree = degree;
+
+		decreaseDegreeElement.style.transform = 'rotate(' + (degree - 180) + 'deg)';
+	}
+
+	function submitCaptcha()
+	{
+		const response = await moFetchApi(
+			'captcha/answerQuestion',
+			{
+				body: JSON.stringify(
+				{
+					degree: imageElement.dataset.degree,
+				}),
+			});
+	}
 }
--- js/infaq.js
+++ js/infaq.js
@@ -1,4 +1,15 @@
 onDOMContentLoaded(async function()
 {
-	await moCreateCaptchaQuestion();
+	var result = await moTryAsync(moCreateCaptchaQuestion, captchaCatchCallback);
+
+	if (result == 1) return;
+	
+	function captchaCatchCallback(error)
+	{
+		const captchaHolder = getElement('captchaHolder');
+
+		captchaHolder.innerHTML = '!';
+
+		console.error(error);
+	}
 });
