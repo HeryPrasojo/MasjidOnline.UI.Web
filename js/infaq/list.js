a04c6eb563ddef0dc7c260a6d4cbe33a9f7d947e
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-04-10 18:21:41 +0700

    feature: initial

--- js/infaq/list.js
+++ js/infaq/list.js
@@ -1,17 +1,57 @@
-mo.onDOMContentLoaded(async function ()
+mo.onDOMContentLoaded(function ()
 {
-    var json = await mo.fetchApiJson(
-        'infaq/infaq/getMany',
-        {
-            body:
-            {
-                page: 1,
-            },
-        });
+    const infaqRowHolder = mo.getElement('infaqRowHolder');
+    const infaqPageCurrent = mo.getElement('infaqPageCurrent');
+    const infaqPageTotal = mo.getElement('infaqPageTotal');
+    const formInfaqPage = mo.getElement('formInfaqPage');
+    const infaqPageInput = mo.getElement('infaqPageInput');
+    const infaqGoButton = mo.getElement('infaqGoButton');
 
-    for (const record of json.records)
+    formInfaqPage.addEventListener('submit', submitForm);
+
+    async function submitForm(e)
     {
-        const tr = document.createElement('tr');
+        e.preventDefault();
+
+        const page = infaqPageInput.value;
+
+        const pageNumber = parseInt(page, 10);
+
+        if (isNaN(pageNumber) || pageNumber < 1) mo.showError('Invalid page number', 'Page number must be a positive integer.');
+
+        infaqRowHolder.innerHTML = '';
+
+        infaqGoButton.disabled = true;
+
+        var json = await mo.fetchApiJson(
+            'infaq/infaq/getMany',
+            {
+                body:
+                {
+                    page: pageNumber,
+                },
+            });
+
+        infaqPageCurrent.innerText = pageNumber;
+        infaqPageTotal.innerText = json.total;
+
+        infaqGoButton.disabled = false;
+
+        for (const record of json.records)
+        {
+            const tr = document.createElement('tr');
+
+            tr.append(
+                document.createElement('td').append(document.createTextNode(record.id)),
+                document.createElement('td').append(document.createTextNode(record.dateTime)),
+                document.createElement('td').append(document.createTextNode(record.munfiqName)),
+                document.createElement('td').append(document.createTextNode(record.amount)),
+                document.createElement('td').append(document.createTextNode(record.status)),
+            );
+
+            infaqRowHolder.append(tr);
+        }
     }
+
     console.log((new Date()).toISOString() + ' load list');
 });
