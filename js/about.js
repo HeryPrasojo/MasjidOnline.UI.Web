c2f2dfad7410f739ca4fac101fbc8fc56a533407
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-04-19 05:32:47 +0700

    feature: initial

--- js/about.js
+++ js/about.js
@@ -1,3 +1,14 @@
-mo.onDOMContentLoaded(function ()
+(function ()
 {
-});
+    if (document.readyState == 'loading')
+        document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+    else
+        onDOMContentLoaded();
+
+    function onDOMContentLoaded()
+    {
+        console.log((new Date()).toISOString() + ' about DOM content loaded');
+    }
+
+    console.log((new Date()).toISOString() + ' about loaded');
+})();
--- js/index.js
+++ js/index.js
@@ -12,4 +12,3 @@
 
     console.log((new Date()).toISOString() + ' index loaded');
 })();
-
