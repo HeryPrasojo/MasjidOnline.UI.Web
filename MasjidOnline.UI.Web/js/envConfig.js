8818f34a256b5cb501a0825886b68e3207d513a7
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-07-12 20:13:29 +0700

    feature: initial

--- MasjidOnline.UI.Web/js/envConfig.js
+++
@@ -1,6 +0,0 @@
-globalThis.mo ??= {};
-
-mo.apiUriPrefix = 'http://api.local.masjidonline.org:5058/';
-mo.environment = 'local';
-mo.recaptchaActionPrefix = 'L_';
-mo.recaptchaSiteKey = '6LdSD_oqAAAAAOS497xVyGNjp5AAN-TpvCxk8b5R';
