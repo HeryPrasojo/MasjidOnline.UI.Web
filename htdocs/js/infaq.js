1443b07106f28d2c91c90f7cd4185d53cb2a152a
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-07-13 22:44:28 +0700

    feature: initial

--- htdocs/js/infaq.js
+++ htdocs/js/infaq.js
@@ -8,7 +8,8 @@
     async function onDOMContentLoaded()
     {
         const urlSearchParams = new URLSearchParams(window.location.search);
-        const id = urlSearchParams.get('id');
+        const idString = urlSearchParams.get('id');
+        const id = parseInt(idString, 10);
 
         var json = await mo.fetchApiJson(
             'infaq/infaq/getOne',
