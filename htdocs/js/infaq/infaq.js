377e842f6039edd790dcd3fe112f469ce6b6360f
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-07-18 09:17:17 +0700

    feature: initial

--- htdocs/js/infaq/infaq.js
+++ htdocs/js/infaq/infaq.js
@@ -7,6 +7,12 @@
 
     async function onDOMContentLoaded()
     {
+        const dateTimeElement = getElementById('dateTime');
+        const munfiqNameElement = getElementById('munfiqName');
+        const paymentElement = getElementById('payment');
+        const amountElement = getElementById('amount');
+        const statusElement = getElementById('status');
+
         const urlSearchParams = new URLSearchParams(window.location.search);
         const idString = urlSearchParams.get('id');
         const id = parseInt(idString, 10);
@@ -19,6 +25,12 @@
                     id: id,
                 },
             });
+
+        dateTimeElement.innerHTML = json.dateTime;
+        munfiqNameElement.innerHTML = json.munfiqName;
+        paymentElement.innerHTML = json.paymentType;
+        amountElement.innerHTML = json.amount;
+        statusElement.innerHTML = json.status;
 
         function getElementById(id)
         {
