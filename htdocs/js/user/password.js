6664867cef8d8c37ee5861430e5a3ec3e9634188
Hery Prasojo <69847809+HeryPrasojo@users.noreply.github.com>
2025-10-30 17:53:57 +0700

    feat: initial

--- htdocs/js/user/password.js
+++ htdocs/js/user/password.js
@@ -1,137 +1,128 @@
-const fetchPremise = import('/js/fetch.js');
-
-if (document.readyState == 'loading')
-    document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
-else
-    onDOMContentLoaded();
-
-async function onDOMContentLoaded()
+(function ()
 {
-    await fetchPremise;
+    const fetchPremise = import('/js/fetch.js');
 
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const passwordCode = urlSearchParams.get('c');
+    if (document.readyState == 'loading')
+        document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+    else
+        onDOMContentLoaded();
 
-    const sessionId = mo.getSession();
-
-    const formElement = getElementById('passwordForm');
-    const passwordElement = getElementById('passwordInput');
-    const password2Element = getElementById('password2Input');
-    const messageElement = getElementById('messageElement');
-    const submitElement = getElementById('submitButton');
-
-    passwordElement.addEventListener('input', validatePasswordInput);
-    password2Element.addEventListener('input', validatePasswordInput);
-
-    // passwordElement.addEventListener('paste', validatePasswordInput);
-    // password2Element.addEventListener('paste', validatePasswordInput);
-
-    formElement.addEventListener('submit', submitForm);
-
-    function validatePasswordInput()
+    async function onDOMContentLoaded()
     {
-        const password = passwordElement.value;
-        const password2 = password2Element.value;
+        await fetchPremise;
 
-        const error = validatePassword(password);
-        if (error)
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const passwordCode = urlSearchParams.get('c');
+
+        const formElement = getElementById('passwordForm');
+        const passwordElement = getElementById('passwordInput');
+        const password2Element = getElementById('password2Input');
+        const messageElement = getElementById('messageElement');
+        const submitElement = getElementById('submitButton');
+
+        passwordElement.addEventListener('input', validatePasswordInput);
+        password2Element.addEventListener('input', validatePasswordInput);
+
+        // passwordElement.addEventListener('paste', validatePasswordInput);
+        // password2Element.addEventListener('paste', validatePasswordInput);
+
+        formElement.addEventListener('submit', submitForm);
+
+        function validatePasswordInput()
         {
-            messageElement.textContent = error;
-            messageElement.style.color = 'red';
-            return;
-        }
+            const password = passwordElement.value;
+            const password2 = password2Element.value;
 
-        if (password !== password2)
-        {
-            messageElement.textContent = 'Passwords should match.';
-            messageElement.style.color = 'red';
-            return;
-        }
-
-        messageElement.textContent = '';
-    }
-
-    async function submitForm(event)
-    {
-        event.preventDefault();
-
-        const password = passwordElement.value;
-        const password2 = password2Element.value;
-
-        if (password == '')
-            return messageElement.textContent = 'Passwords should not empty.';
-
-        const passwordValidationResult = validatePassword(password);
-        if (passwordValidationResult)
-            return messageElement.textContent = passwordValidationResult;
-
-        if (password != password2)
-            return messageElement.textContent = 'Passwords should match.';
-
-
-        submitElement.disabled = true;
-        submitElement.classList.toggle("loading");
-
-
-        var captchaToken;
-
-        const sessionId = mo.getSession();
-
-        if (!sessionId)
-            captchaToken = await grecaptcha.enterprise.execute(mo.recaptchaSiteKey, { action: 'infaq' + mo.recaptchaActionAffix });
-
-        var json = await mo.fetchApiJson(
-            'user/setPassword',
+            const error = validatePassword(password);
+            if (error)
             {
-                body:
-                {
-                    captchaToken,
-                    passwordCode,
-                    password,
-                    password2,
-                },
-            });
+                messageElement.textContent = error;
+                messageElement.style.color = 'red';
+                return;
+            }
 
-        if (json.resultCode)
-        {
-            submitElement.classList.toggle("loading");
-            submitElement.disabled = false;
+            if (password !== password2)
+            {
+                messageElement.textContent = 'Passwords should match.';
+                messageElement.style.color = 'red';
+                return;
+            }
 
-            return messageElement.textContent = json.resultMessage;
+            messageElement.textContent = '';
         }
 
-        if (!sessionId)
-            mo.setSession(json.data);
+        async function submitForm(event)
+        {
+            event.preventDefault();
 
-        mo.setLoggedIn();
+            const password = passwordElement.value;
+            const password2 = password2Element.value;
 
-        messageElement.textContent = 'Success, redirecting...';
+            if (password == '')
+                return messageElement.textContent = 'Passwords should not empty.';
 
-        location.href = '/';
+            const passwordValidationResult = validatePassword(password);
+            if (passwordValidationResult)
+                return messageElement.textContent = passwordValidationResult;
+
+            if (password != password2)
+                return messageElement.textContent = 'Passwords should match.';
+
+
+            submitElement.disabled = true;
+            submitElement.classList.toggle("loading");
+
+
+            const json = await mo.fetchApiJson(
+                'user/setPassword',
+                {
+                    body:
+                    {
+                        captchaToken: await grecaptcha.enterprise.execute(mo.recaptchaSiteKey, { action: 'infaq' + mo.recaptchaActionAffix }),
+                        passwordCode,
+                        password,
+                        password2,
+                    },
+                });
+
+            if (json.resultCode)
+            {
+                submitElement.classList.toggle("loading");
+                submitElement.disabled = false;
+
+                return messageElement.textContent = json.resultMessage;
+            }
+
+            mo.setLoggedIn();
+
+            messageElement.textContent = 'Success, redirecting...';
+
+            location.href = '/';
+        }
+
+        function getElementById(id)
+        {
+            return document.getElementById(id);
+        }
+
+        function validatePassword(password)
+        {
+            if (password.length < 8)
+                return 'Password must be at least 8 characters long';
+
+            if (!/[a-z]/.test(password))
+                return 'Password must contain at least one lowercase letter';
+
+            if (!/[A-Z]/.test(password))
+                return 'Password must contain at least one uppercase letter';
+
+            if (!/\d/.test(password))
+                return 'Password must contain at least one digit';
+
+            if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password))
+                return 'Password must contain at least one symbol';
+
+            return null;
+        }
     }
-
-    function getElementById(id)
-    {
-        return document.getElementById(id);
-    }
-
-    function validatePassword(password)
-    {
-        if (password.length < 8)
-            return 'Password must be at least 8 characters long';
-
-        if (!/[a-z]/.test(password))
-            return 'Password must contain at least one lowercase letter';
-
-        if (!/[A-Z]/.test(password))
-            return 'Password must contain at least one uppercase letter';
-
-        if (!/\d/.test(password))
-            return 'Password must contain at least one digit';
-
-        if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password))
-            return 'Password must contain at least one symbol';
-
-        return null;
-    }
-}
+})();
